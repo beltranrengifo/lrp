0028b9ee6503fe32da03395058cafa729ec1e41f
Beltrán <merloc@gmail.com>
2021-06-27 13:27:59 +0200

    add configs

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+  },
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+}
